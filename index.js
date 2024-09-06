e24f1021fefa0a044eb36b71c0200274da0c6541
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2024-09-06 22:01:34 +0200

    Added elements for our crypto currencies. use the Data points provided by scrimba and changed it to Zar and dollar sign to R.

--- index.js
+++ index.js
@@ -8,7 +8,25 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
   document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1517200578024-62d131797ec8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjU2NDkyMzV8&ixlib=rb-4.0.3&q=80&w=1080)`
 })
 
-fetch ("https://api/coingecko/com/api/v3/coins/dogecoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+      
+
+      document.getElementById("crypto").innerHTML = ` 
+       <img src=${data.image.small} />
+      <span>${data.name}</span`
+
+     document.getElementById("crypto").innerHTML += `
+      <p>🎯: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar} </p>
+     `
+
+    })
     .catch(err => console.error(err))
