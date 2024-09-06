29b340d691e5eb2932767414797edfd85ff720e3
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2024-09-06 21:32:20 +0200

    Added crypto currency for coingecko api

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ fetch ("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&qu
 .then(data => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById("author").textContent =`By: ${data.user.name}` 
+}) 
+.catch (error => {
+  document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1517200578024-62d131797ec8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjU2NDkyMzV8&ixlib=rb-4.0.3&q=80&w=1080)`
 })
+
+fetch ("https://api/coingecko/com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
